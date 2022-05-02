e0192f263209a309969d16aaa5cc9ce722a5b029
Felipe Castro <castrofe55@gmail.com>
2022-05-02 03:00:33 -0300

    added some components and context

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/react";
+
+export function Logo() {
+  return (
+    <Text fontSize={["2xl", "3xl"]} fontWeight="bold" letterSpacing="tight" w="64">
+      boardash
+      <Text as="span" ml="1" color="purple.500">.</Text>
+    </Text>
+  );
+};
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,17 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
+import { RiAddLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -17,31 +23,32 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="purple"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar novo usuário
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="purple"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo usuário
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="purple" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="8"></Th>
+                { isWideVersion && <Th>Data de cadastro</Th> }
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="purple" />
                 </Td>
                 <Td>
@@ -50,24 +57,13 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">flpc.dev@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>23 de Maio, 2022</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                { isWideVersion &&  <Td>23 de Maio, 2022</Td> }
               </Tr>
             </Tbody>
 
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="purple" />
                 </Td>
                 <Td>
@@ -76,24 +72,13 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">flpc.dev@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>23 de Maio, 2022</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                { isWideVersion &&  <Td>23 de Maio, 2022</Td> }
               </Tr>
             </Tbody>
 
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="purple" />
                 </Td>
                 <Td>
@@ -102,18 +87,7 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">flpc.dev@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>23 de Maio, 2022</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                { isWideVersion &&  <Td>23 de Maio, 2022</Td> }
               </Tr>
             </Tbody>
           </Table>
